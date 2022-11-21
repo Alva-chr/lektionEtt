d5dbb5c6e7a1d02e3bde439ec679f362e7f6284d
alvaDenBasta <alvachristensson03@gmail.com>
2022-11-21 14:15:05 +0100

    klar med obligatoriskt

--- lektion6/smooth.py
+++ lektion6/smooth.py
@@ -1,30 +1,32 @@
-def smooth(a):
-    res = []
-    res.append(a[0])
-    for i in range(1, len(a)-1):
-        res.append(sum(a[i-1:i+2])/3)
-    res.append(a[-1])
-    return res
-
 def smooth_a(a, n):
     res = []
-    N = len(a) - 1
+    len_a = len(a)
+    a = [a[0] for i in range(n)] + a + [a[-1] for i in range(n)]
 
-   # for i in range(0, N):
-    #    res.append(sum(a[max(i-n,0):min(i+n,N)])/(2*n +1))
-
-    for i in range(len(a)):
-        k = 0
-        for j in range(2*n+1):
-            k = sum(a[max(i-j,0):min(i+j,N)])
-
-        res.append(k/(2*n+1))
+    for i in range(n, len_a+n):
+        x = a[i-n:i+n+1]
+        res.append(sum(x)/len(x))
 
     return res
+
+def smooth_b(a, n):
+    res = []
+
+    for i in range(len(a)):
+        b = a[max(i-n,0):min(i+n+1,len(a))]
+        res.append(sum(b)/len(b))
+
+    return res
+
+def round_list(a_list, ndigits):
+    return [round(i, ndigits) for i in a_list]
+
 
 x = [1, 2, 6, 4, 5, 0, 1, 2]
 print('smooth_a(x, 1): ', smooth_a(x, 1))
 print('smooth_a(x, 2): ', smooth_a(x, 2))
+print('smooth_b(x, 1): ', smooth_b(x, 1))
+print('smooth_b(x, 2): ', smooth_b(x, 2))
 
 #smooth_a(x, 1): [1.3333333333333333, 3.0, 4.0, 5.0, 3.0, 2.0, 1.0, 1.6666666666666667] 
 #smooth_a(x, 2): [2.2, 2.8, 3.6, 3.4, 3.2, 2.4, 2.0, 1.4]
